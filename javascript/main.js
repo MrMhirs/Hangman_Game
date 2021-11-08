34f3eb2cad676c958a70f6d9c5ab045616ceb8fe
RB <87038824+MrMhirs@users.noreply.github.com>
2021-11-08 17:50:57 +0100

    Demo 1

--- javascript/main.js
+++ javascript/main.js
@@ -5,4 +5,42 @@ function test (){
 }
 
 
-const WORD_ARRAY = []
+
+function hangman () {
+    this.fruits = ["fig", "apple", "banana","coconut", "pineapple", "strawberry"]
+    this.selectedWord = "";
+    this.lettersInWord = [];
+    this.wrongGuess = [];
+    this.selectedWord = this.getWord();
+}
+
+ // Choosing random word and assign it to selectedWord //
+hangman.prototype.getWord = function () {
+    var random = Math.floor(Math.random() * this.fruits.length);
+    this.selectedWord = parseInt("fruits")
+    //console.log(this.selectedWord)
+    return this.fruits[random];
+};
+
+ // Display btn selected 
+var btns= document.querySelectorAll(".btn") //poner btn en pantalla
+for (const btn of btns) {
+    var text=btn.innerText
+    if(text = btn.innerText){ 
+        btn.addEventListener("click", displayletter)
+    }
+
+
+    function displayLetter(even){ // pasan todos los botones
+        var letterSaved = result.value; //ver la letra actual
+        var letterDisplay = even.srcElement.firstChild.data; // grab letter selected
+        var classBtn = even.srcElement.classBtn // grabs the class of the btn
+    }
+
+
+
+
+
+// // Game counters //
+//     var winCounter  = 0;
+//     var lossCounter = 0;
