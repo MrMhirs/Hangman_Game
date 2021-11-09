56537f2ad4b3eae0ed39b8adc87864d68e55e049
MarcelDurganteDev <info@marceldurgante.dev>
2021-11-09 17:43:35 +0100

    created JS event listener for each letter on keyboard to remove on click and append to used letters container

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,17 +1,44 @@
-let lettersContainer = document.querySelector( '.letters-container' );
+const keyboard = document.querySelector( '#injectLetters' );
 
 let alphabet;
 let letters;
-let letter;
+let letterBtn;
+let lettersClicked = [];
 
 // FOR LOOP TO GENERATE THE ALPHABET FROM "A" TO "Z"
 for ( i = 9, alphabet = ''; ++i < 36; ) {
     alphabet += i.toString( 36 );
 }
+
 letters = new Array (...alphabet);
+
 // FOR LOOP TO CREATE BUTTONS FOR EACH LETTER AND APPEND TO DIV PARENT AND ADD INNER HTML
-for (i = 0; i < letters.length; i++) {
-    letter = document.createElement('button');
-    lettersContainer.appendChild( letter);
-    letter.innerHTML = letters[i];
+
+for (i in letters/* i = 0; i < letters.length; i++ */) {
+    letterBtn = document.createElement('button');
+    letterBtn.setAttribute('id', letters[i]);
+    letterBtn.setAttribute('value', letters[i]);
+    letterBtn.setAttribute('class', 'btn-letters')
+    letterBtn.innerHTML = letters[i];
+    keyboard.appendChild( letterBtn );
 }
+
+let btnLetters = document.querySelectorAll('.btn-letters');
+const usedLetters = document.querySelector('.clicked-letters')
+
+btnLetters.forEach(element => {
+    var btnContent = element.innerHTML;
+    element.addEventListener('click', () => {
+    keyboard.removeChild(element);
+    usedLetters.appendChild(element)
+
+    })
+});
+
+
+
+
+// CLICKED LETTERS NO KEYBOARD DELETED FROM KEYBOARD
+
+
+
