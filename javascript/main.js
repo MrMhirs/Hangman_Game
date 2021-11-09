0d972cc49c099a8e132fbdc0f1c6dc8e08f8112b
RB <87038824+MrMhirs@users.noreply.github.com>
2021-11-09 17:42:44 +0100

    Functions

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,14 @@
-function test (){
+function playerName (){
     const playerName = document.getElementById("username")
     localStorage.setItem("playerName",playerName.value)
     console.log(localStorage.getItem("playerName"))
 }
+
+var currentWord = []
+var answerWord = []
+var pastLetters = []
+var lettersReset = "";
+var guessesLeft = 10;
 
 const fruits = [
     "fig",
@@ -11,47 +17,81 @@ const fruits = [
     "coconut",
     "pineapple",
     "strawberry"
-]
+];
 
-var answer = "";
-var mistakes = 0;
-var guessed = [];
-var wordStatus = null;
-document.getElementById("key").innerHTML
-
-
-function randomWord() {
-    answer = fruits[Math.floor(Math.random() * fruits.length)];
+var answer = fruits[Math.floor(Math.random() * fruits.length)];
     console.log(answer)
-}
 
+document.getElementById("guesses-remain").innerHTML = guessesLeft;
 
+var wins = 0;
+document.getElementById("num-of-wins").innerHTML = wins;
 
-function guessedLetter(chosenKey) {
-    guessed.indexOf(chosenKey) === -1 ? guessed.push(chosenKey) : null;
-    document.getElementById(chosenKey).setAttribute("disable", true);
-    alert(answer);
+// underscores of the word
+for (i = 0; i < answer.length; i++) {
+    currentWord.push("_");
+  }
+  
+document.getElementById("theWord").innerHTML = currentWord.join(" ");
 
-
-    if (answer.indexOf(chosenKey) >=0) {
-        guessedWord();
+//Evaluate the position of a letter in the word
+function wordLetters(letter){
+    var letterPos = new Array();
+    for ( i = 0; i < answer.length; i++){
+        if (answer[i] === letter)
+        letterPos.push (i);
+    
     }
+    return letterPos;
 }
 
 
-
-// Check if the letter exist in guessed array, if it does > 0 = true or false (.join= no commas)
-function guessedWord() {
-    wordStatus = answer.split("").map(key => (guessed.indexOf(fruits) >= 0 ? key : " _ ")).join("");
-
-    document.getElementById("theWord").innerHTML = wordStatus
+//  Return letters that are not guessed
+function lettersToGuess () {
+    var i ;
+    var toGuess = 0;
+    for ( i in currentWord) {
+        if (currentWord[i] === "_")
+        toGuess++;
+    }
+    return toGuess;
 }
 
 
+// function pressedKey (word, letter) {
+//     let pressedKey = document.getElementById ("key").innerHTML = guessesLeft;
+// }
+
+
+
+document.onkeyup = function (event) {
+    var letter =  event.key.toLowerCase();
+    var lettersGuessed = letter;
+    var letterPos = wordLetters;
+
+    console.log(event)
 
 
 
 
-randomWord();
-guessedWord();
+        var letter = event.key.toLowerCase();
+        var lettersGuessed = letter;
+        var i;
+        var letterPos = wordLetters(lettersGuessed);
 
+        if (letterPos.length) {
+            for (i = 0; i < letterPos.length; i++) {
+            currentWord[letterPos[i]] = lettersGuessed;
+}
+        document.getElementById("active-word").innerHTML = currentWord.join(" ");
+
+            } else {
+            // If the letter has already been seen don't do it again.
+                if (!pastLetters.includes(letter)) {
+                // Placing the letter into an array that we can reference outside the scope of the key up event.
+                pastLetters.push(letter);
+                document.getElementById("letters-guessed").innerHTML += lettersGuessed + " ";
+                guessesLeft--;
+                document.getElementById("guesses-remain").innerHTML = guessesLeft;
+                }
+            }
