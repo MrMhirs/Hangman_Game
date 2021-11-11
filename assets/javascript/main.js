fb996d38bec61775b881d686be8fbb8f746e575a
MarcelDurganteDev <info@marceldurgante.dev>
2021-11-11 16:25:47 +0100

    JS DATA file done, create user and store or retrieve from localStorage

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,55 +1,11 @@
-// Using LocalStorage to save players name//
-function playerName (){
-    const playerName = document.getElementById("userName")
-    localStorage.setItem("playerName",playerName.value)
-    console.log(localStorage.getItem("playerName"))
-}
-
+//  variables and constants declarations
 
 const keyboard = document.querySelector( '#injectLetters' );
-
-let alphabet;
-let letters;
-let letterBtn;
-let lettersClicked = [];
-
-// FOR LOOP TO GENERATE THE ALPHABET FROM "A" TO "Z"
-for ( i = 9, alphabet = ''; ++i < 36; ) {
-    alphabet += i.toString( 36 );
-}
-
-letters = new Array (...alphabet);
-
-// FOR LOOP TO CREATE BUTTONS FOR EACH LETTER AND APPEND TO DIV PARENT AND ADD INNER HTML
-
-for (i in letters/* i = 0; i < letters.length; i++ */) {
-    letterBtn = document.createElement('button');
-    letterBtn.setAttribute('id', letters[i]);
-    letterBtn.setAttribute('value', letters[i]);
-    letterBtn.setAttribute('class', 'btn-letters')
-    letterBtn.innerHTML = letters[i];
-    keyboard.appendChild( letterBtn );
-}
-
-let btnLetters = document.querySelectorAll('.btn-letters');
-const usedLetters = document.querySelector('.clicked-letters')
-
-// CLICKED LETTERS NO KEYBOARD DELETED FROM KEYBOARD
-btnLetters.forEach(element => {
-    var btnContent = element.innerHTML;
-    element.addEventListener('click', () => {
-    keyboard.removeChild(element);
-    usedLetters.appendChild(element)
-
-    })
-});
-
-
-var currentWord = []
-var answerWord = []
-var lettersReset = "";
-
-
+const usedLetters = document.querySelector( '.clicked-letters' );
+const startGameBtn = document.getElementById( 'startGameBtn' );
+const lettersClicked = [];
+const currentWord = [];
+const answerWord = [];
 const fruits = [
     "fig",
     "apple",
@@ -58,19 +14,54 @@ const fruits = [
     "pineapple",
     "strawberry"
 ];
+const HISTORY = [];
+let players = [];
 
-// Selecting random word //
-var answer = fruits[Math.floor(Math.random() * fruits.length)];
-    console.log(answer)
+const answer = fruits[Math.floor( Math.random() * fruits.length )]; // Selecting random word from fruits array
+let alphabet;
+let letters;
+let letterBtn;
+let lettersReset = "";
+let btnLetters;
+let username;
+let currId = 0;
+
+let playersIds;
+
+// For loop to generate the alphabet from "A" to "Z"
+
+letters = new Array( ...alphabet );
+
+for ( i = 9, alphabet = ''; ++i < 36; ) {
+    alphabet += i.toString( 36 );
+}
+
+
+// For loop to create buttons for each letter and append them to div parent and add innerHTML
+for ( i in letters/* i = 0; i < letters.length; i++ */ ) {
+    btnLetters = document.querySelectorAll( '.btn-letters' );
+    letterBtn = document.createElement( 'button' );
+    letterBtn.setAttribute( 'id', letters[i] );
+    letterBtn.setAttribute( 'value', letters[i] );
+    letterBtn.setAttribute( 'class', 'btn-letters' );
+    letterBtn.innerHTML = letters[i];
+    keyboard.appendChild( letterBtn );
+}
+
+// clicked letters are deleted from keyboard
+btnLetters.forEach( element => {
+    let btnContent = element.innerHTML;
+    element.addEventListener( 'click', () => {
+        keyboard.removeChild( element );
+        usedLetters.appendChild( element );
+    } );
+} );
+
+
 
 // Underscores of the word
-for (i = 0; i < answer.length; i++) {
-    currentWord.push("_");
-  }
-  document.getElementById("underLine").innerHTML = currentWord.join(" ");
-
-
-
-
-
+for ( i = 0; i < answer.length; i++ ) {
+    currentWord.push( "_" );
+}
+document.getElementById( "underLine" ).innerHTML = currentWord.join( " " );
 
