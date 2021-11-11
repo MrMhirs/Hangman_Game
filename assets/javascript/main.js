7b15fb015f1d7df7bacc611dbdb6fad965b3521a
MarcelDurganteDev <info@marceldurgante.dev>
2021-11-11 17:39:28 +0100

    js update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -41,124 +41,122 @@ for ( i = 9, alphabet = ''; ++i < 36; ) {
 let btnLetters = document.querySelectorAll( '.btn-letters' );
 
 for ( i = 0; i < letters.length; i++ ) {
-   /*  btnLetters = document.querySelectorAll( '.btn-letters' ); */
+    /*  btnLetters = document.querySelectorAll( '.btn-letters' ); */
     letterBtn.setAttribute( 'id', letters[i] );
     letterBtn.setAttribute( 'value', letters[i] );
     letterBtn.setAttribute( 'class', 'btn-letters' );
     letterBtn.innerHTML = letters[i];
     keyboard.appendChild( letterBtn );
 
-let btnLetters = document.querySelectorAll('.btn-letters');
-const usedLetters = document.querySelector('.clicked-letters')
+    let btnLetters = document.querySelectorAll( '.btn-letters' );
+    const usedLetters = document.querySelector( '.clicked-letters' );
 
 
-// Selecting random word //
-var answer = fruits[Math.floor(Math.random() * fruits.length)];
+    // Selecting random word //
+    var answer = fruits[Math.floor( Math.random() * fruits.length )];
 
-// Underscores for the word
-for (i = 0; i < answer.length; i++) {
-    currentWord.push("_");
-}
-document.getElementById("underLine").innerHTML = currentWord.join(" ");
-
-
-// CLICKED LETTERS NO KEYBOARD DELETED FROM KEYBOARD
-btnLetters.forEach(element => {
-    var btnContent = element.innerHTML;
-    element.addEventListener('click', () => {
-    keyboard.removeChild(element);
-    usedLetters.appendChild(element)
-    checkIfPresent(element.value);
-    })
-});
-
-function checkIfPresent(letter) {
-    const aux = answer.split('')
-    console.log(currentWord.join(""))
-    if (currentWord.join("") == answer){
-
-        youWin();
+    // Underscores for the word
+    for ( i = 0; i < answer.length; i++ ) {
+        currentWord.push( "_" );
     }
-    console.log(aux)
-    if (!aux.includes(letter))
-    changeLifeGame();
+    document.getElementById( "underLine" ).innerHTML = currentWord.join( " " );
+
+
+    // CLICKED LETTERS NO KEYBOARD DELETED FROM KEYBOARD
+    btnLetters.forEach( element => {
+        var btnContent = element.innerHTML;
+        element.addEventListener( 'click', () => {
+            keyboard.removeChild( element );
+            usedLetters.appendChild( element );
+            checkIfPresent( element.value );
+        } );
+    } );
+
+    function checkIfPresent ( letter ) {
+        const aux = answer.split( '' );
+        console.log( currentWord.join( "" ) );
+        if ( currentWord.join( "" ) == answer ) {
+
+            youWin();
+        }
+        console.log( aux );
+        if ( !aux.includes( letter ) )
+            changeLifeGame();
 
 
 
 
-    while (aux.includes(letter)){
-        var index = aux.indexOf(letter)
-        delete aux[index]
-        currentWord[index] = letter;
-        console.log(index)
+        while ( aux.includes( letter ) ) {
+            var index = aux.indexOf( letter );
+            delete aux[index];
+            currentWord[index] = letter;
+            console.log( index );
+        }
+        if ( currentWord.join( "" ) == answer ) {
+
+            youWin();
+        }
+
+        console.log( currentWord );
+        document.getElementById( "underLine" ).innerHTML = currentWord.join( " " );
     }
-    if (currentWord.join("") == answer){
 
-        youWin();
+
+    const figureParts = document.querySelectorAll( ".figure-part" );
+
+    // Display parts
+
+    var countLife = 7;
+
+    function changeLifeGame () {
+        countLife--;
+        console.log( countLife );
+        switch ( countLife ) {
+            case 6:
+                document.getElementById( "head" ).classList.remove( "figure-part" );
+
+                break;
+
+            case 5:
+                document.getElementById( "body" ).classList.remove( "figure-part" );
+                break;
+
+            case 4:
+                document.getElementById( "armL" ).classList.remove( "figure-part" );
+                break;
+
+            case 3:
+                document.getElementById( "armR" ).classList.remove( "figure-part" );
+                break;
+
+            case 2:
+                document.getElementById( "legL" ).classList.remove( "figure-part" );
+                break;
+
+            case 1:
+                document.getElementById( "legR" ).classList.remove( "figure-part" );
+                break;
+
+            case 0:
+                document.getElementsByClassName( "lost-page" )[0].scrollIntoView();
+                countLife = 0;
+        }
     }
 
-    console.log(currentWord)
-    document.getElementById("underLine").innerHTML = currentWord.join(" ");
-}
-
-
-const figureParts = document.querySelectorAll(".figure-part");
-
-// Display parts
-
-var countLife = 7
-
-function changeLifeGame() {
-    countLife--
-    console.log(countLife)
-    switch (countLife) {
-        case 6:
-            document.getElementById("head").classList.remove("figure-part");
-
-            break;
-
-        case 5:
-            document.getElementById("body").classList.remove("figure-part");
-            break;
-
-        case 4:
-            document.getElementById("armL").classList.remove("figure-part");
-            break;
-
-        case 3:
-            document.getElementById("armR").classList.remove("figure-part");
-            break;
-
-        case 2:
-            document.getElementById("legL").classList.remove("figure-part");
-            break;
-
-        case 1:
-            document.getElementById("legR").classList.remove("figure-part");
-            break;
-
-        case 0:
-            document.getElementsByClassName("lost-page")[0].scrollIntoView();
-            countLife = 0
+    function youWin () {
+        document.getElementsByClassName( "won-page" )[0].scrollIntoView();
     }
-}
-
-function youWin() {
-    document.getElementsByClassName("won-page")[0].scrollIntoView();
-}
 
     // all users
 
-// user Data
+    // user Data
 
     function player ( name, idade, total ) {
-
         return {
             username: name,
             age: idade,
             score: total,
         };
-
     };
 
     function getUser () {
@@ -173,4 +171,4 @@ function youWin() {
         userInputs = player( username, age, score );
         allPlayers.push( userInputs );
         localStorage.setItem( "allPlayers", JSON.stringify( allPlayers ) );
-    }
+    };
