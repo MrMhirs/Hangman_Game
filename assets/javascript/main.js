685b9b33fd59208c04dfb7a14960b6ba1cc38fc2
MarcelDurganteDev <info@marceldurgante.dev>
2021-11-17 09:39:43 +0100

    factorization

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,21 +1,55 @@
-// Using LocalStorage to save players name//
-function playerName (){
-    const playerName = document.getElementById("userName")
-    localStorage.setItem("playerName",playerName.value)
-    console.log(localStorage.getItem("playerName"))
+// start game button
+const startGameBtn = document.getElementById('startGameBtn');
+startGameBtn.addEventListener('click', () => {
+    createPlayer();
+    displayScores();
+    console.log('hi)');
+});
+
+// Using LocalStorage to save players data//
+var usernameInput = document.getElementById('playerName');
+// check localStorage for players data
+var scoreboard = document.querySelectorAll('.scoreboard');
+console.log(scoreboard);
+
+function displayScores() {
+    var players = JSON.parse(localStorage.getItem('players'));
+    console.log(players);
+    console.log(players.username);
+    var elSpan = document.createElement('p');
+    elSpan.innerHTML = players.username;
+    for(let i=0; i<scoreboard.length; i++){
+        scoreboard.appendChild(elSpan);
+     
+       }
 }
 
+function createPlayer() {
+    // get values from input
+    var username = usernameInput.value.trim();
+    if (!username) {
+        return;
+    }
+    // save it in local localStorage
+    var players = {
+        username: username,
+        score: score
+    };
+
+    localStorage.setItem('players', JSON.stringify(players));
+   
+
 const fruits = [
-    "fig",
-    "apple",
-    "banana",
-    "coconut",
-    "pineapple",
-    "strawberry",
-    "pomegranate"
+    'fig',
+    'apple',
+    'banana',
+    'coconut',
+    'pineapple',
+    'strawberry',
+    'pomegranate'
 ];
 
-const keyboard = document.querySelector( '#injectLetters' );
+const keyboard = document.querySelector('#injectLetters');
 
 let alphabet;
 let letters;
@@ -23,127 +57,130 @@ let letterBtn;
 let lettersClicked = [];
 
 // FOR LOOP TO GENERATE THE ALPHABET FROM "A" TO "Z"
-for ( i = 9, alphabet = ''; ++i < 36; ) {
-    alphabet += i.toString( 36 );
+for (i = 9, alphabet = ''; ++i < 36; ) {
+    alphabet += i.toString(36);
 }
 
-letters = new Array (...alphabet);
+letters = new Array(...alphabet);
 
 // FOR LOOP TO CREATE BUTTONS FOR EACH LETTER AND APPEND TO DIV PARENT AND ADD INNER HTML
 
-for (i in letters/* i = 0; i < letters.length; i++ */) {
+for (i in letters /* i = 0; i < letters.length; i++ */) {
     letterBtn = document.createElement('button');
     letterBtn.setAttribute('id', letters[i]);
     letterBtn.setAttribute('value', letters[i]);
-    letterBtn.setAttribute('class', 'btn-letters')
+    letterBtn.setAttribute('class', 'btn-letters');
     letterBtn.innerHTML = letters[i];
-    keyboard.appendChild( letterBtn );
+    keyboard.appendChild(letterBtn);
 }
 
 let btnLetters = document.querySelectorAll('.btn-letters');
-const usedLetters = document.querySelector('.clicked-letters')
+const usedLetters = document.querySelector('.clicked-letters');
 
-
-var currentWord = []
+var currentWord = [];
 // Selecting random word //
 var answer = fruits[Math.floor(Math.random() * fruits.length)];
-    console.log(answer)
-
 // Underscores for the word
 for (i = 0; i < answer.length; i++) {
-    currentWord.push("_");
+    currentWord.push('_');
 }
-document.getElementById("underLine").innerHTML = currentWord.join(" ");
-
+document.getElementById('underLine').innerHTML = currentWord.join(' ');
 
 // CLICKED LETTERS NO KEYBOARD DELETED FROM KEYBOARD
 btnLetters.forEach(element => {
     var btnContent = element.innerHTML;
     element.addEventListener('click', () => {
-    keyboard.removeChild(element);
-    usedLetters.appendChild(element)
-    checkIfPresent(element.value);
-    })
+        keyboard.removeChild(element);
+        usedLetters.appendChild(element);
+        checkIfPresent(element.value);
+    });
 });
 
 function checkIfPresent(letter) {
-    const aux = answer.split('')
-    console.log(currentWord.join(""))
-    console.log(answer)
-    if (currentWord.join("") == answer){
+    const aux = answer.split('');
 
+    if (currentWord.join('') == answer) {
         youWin();
     }
-    console.log(aux)
-    if (!aux.includes(letter))
-    changeLifeGame();
+    if (!aux.includes(letter)) changeLifeGame();
 
-
-
-
-    while (aux.includes(letter)){
-        var index = aux.indexOf(letter)
-        delete aux[index]
+    while (aux.includes(letter)) {
+        var index = aux.indexOf(letter);
+        delete aux[index];
         currentWord[index] = letter;
-        console.log(index)
-    }
-    if (currentWord.join("") == answer){
-    
-        youWin();
     }
 
-    console.log(currentWord)
-    document.getElementById("underLine").innerHTML = currentWord.join(" ");
+    if (currentWord.join('') == answer) {
+        youWin();
+    }
+    document.getElementById('underLine').innerHTML = currentWord.join(' ');
 }
 
-
-const figureParts = document.querySelectorAll(".figure-part");
-
 // Display parts
+var countLife = 6;
+var score = 60;
 
-var countLife = 6
+let head = document.getElementById('head');
+let body = document.getElementById('body');
+let armL = document.getElementById('armL');
+let armR = document.getElementById('armR');
+let legL = document.getElementById('legL');
+let legR = document.getElementById('legR');
 
 function changeLifeGame() {
-    countLife--
-    console.log(countLife)
+    countLife--;
+    score = score - 10;
     switch (countLife) {
         case 5:
-            document.getElementById("head").classList.remove("figure-part");
-
+            head.classList.remove('figure-part');
             break;
 
         case 4:
-            document.getElementById("body").classList.remove("figure-part");
+            body.classList.remove('figure-part');
             break;
 
         case 3:
-            document.getElementById("armL").classList.remove("figure-part");
+            armL.classList.remove('figure-part');
             break;
 
         case 2:
-            document.getElementById("armR").classList.remove("figure-part");
+            armR.classList.remove('figure-part');
             break;
 
         case 1:
-            document.getElementById("legL").classList.remove("figure-part");
+            legL.classList.remove('figure-part');
             break;
 
         case 0:
-            document.getElementById("legR").classList.remove("figure-part");
-            document.getElementsByClassName("lost-page")[0].scrollIntoView();
-            countLife = 0
+            legR.classList.remove('figure-part');
+            youLost();
             break;
     }
 }
 
 function youWin() {
-    document.getElementsByClassName("won-page")[0].scrollIntoView();
+    document.getElementsByClassName('won-page')[0].scrollIntoView();
 }
 
-var playAgainBtn = document.querySelectorAll(".playAgain")
-    playAgainBtn.forEach(element => {
-        console.log(element)
-        element.addEventListener("click", () => {
-            document.getElementsByClassName("first-page")[0].scrollIntoView();
-        });
+function youLost() {
+    document.getElementsByClassName('lost-page')[0].scrollIntoView();
+    countLife = 0;
+}
+
+var playAgainBtn = document.querySelectorAll('.playAgain');
+playAgainBtn.forEach(element => {
+    element.addEventListener('click', () => {
+        document.getElementsByClassName('first-page')[0].scrollIntoView();
+        hideHangman();
     });
+});
+
+function hideHangman() {
+    head.classList.add('figure-part');
+    body.classList.add('figure-part');
+    armL.classList.add('figure-part');
+    armR.classList.add('figure-part');
+    legL.classList.add('figure-part');
+    legR.classList.add('figure-part');
+    location.reload();
+}
